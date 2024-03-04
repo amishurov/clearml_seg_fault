2a9b9bb757f845764d6b463711fad2339655199c
Alexander Mishurov <al.mishurov@gmail.com>
2024-03-04 11:06:29 +0300

    Segfault fixed

--- tests/test_typing.py
+++ tests/test_typing.py
@@ -1,9 +1,20 @@
+import argparse
+import importlib
 import sys
+from contextlib import contextmanager
+
 import mypy.api
 
+@contextmanager
+def reimport_argparse():
+    imported_argparse = sys.modules.get('argparse')
+    if imported_argparse and hasattr(imported_argparse.ArgumentParser, '_parse_args_patched'):
+        importlib.reload(argparse)
+    yield
 
 def test_typing():
-    out, err, exit_code = mypy.api.run(['--config-file', 'pyproject.toml'])
-    sys.stdout.write(out)
-    sys.stderr.write(err)
-    assert 0 == exit_code, out
+    with reimport_argparse():
+        out, err, exit_code = mypy.api.run(['--config-file', 'pyproject.toml'])
+        sys.stdout.write(out)
+        sys.stderr.write(err)
+        assert 0 == exit_code, out
